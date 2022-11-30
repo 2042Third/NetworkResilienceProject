ee7fe00690825a5125e73236efcbd938e4aa616a
mike yang <18604713262@163.com>
2022-11-29 20:48:45 -0500

    Restarting connected components

--- src/Graph.h
+++ src/Graph.h
@@ -22,8 +22,6 @@ namespace NetworkResilience {
     DegreeDistro* dd = nullptr; // Cached degree distribution
     ConnectedComps* cc = nullptr; // Cached connected components
 
-    void add_connected(const size_t &a,const basic_string<char> &b);
-    int add_connected(const basic_string<char> &a);
     Graph(double NIn, double pIn);
     Graph(Graph const &graph);
     ConnectedComp* iterateConnectedComponent(const ConnectedComp& inputC);
@@ -33,12 +31,9 @@ namespace NetworkResilience {
     int rmNode(const NODE_ID& nodeId);
     void printStats(const DegreeDistro& m) const;
 
-    void iterate_nodes(const NODE_ID& n) ;
     std::random_device rd;   // non-deterministic generator
     crypto *gen = nullptr;
     ~Graph();
-    static void merge_cc(ConnectedComp&a,ConnectedComp&b);
-    void check_connected( );
 
   protected:
     unordered_set<NODE_ID> itrd;
